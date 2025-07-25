c753a530534cfe51c6ef9e29177e6da89b77f0b7
Carlos Henrique Araújo <151400787+HenriqueAraujo1@users.noreply.github.com>
2025-07-25 08:25:37 -0300

    Comentarios produtos.ts

--- src/routes/produtos.ts
+++ src/routes/produtos.ts
@@ -1,15 +1,15 @@
-import e, { Router } from 'express';
-import connection from '../config/database';
+import e, { Router } from 'express'; // importando a rota de routes    
+import connection from '../config/database'; // importando a conexão do banco de dados
 
-const router = Router();
-
+const router = Router(); // instanciando a rota 
+ 
 // Rota para obter todos os produtos
 router.get('/produtos', (req, res) => {
-    connection.query('SELECT * FROM produtos', (error, results) => {
+    connection.query('SELECT * FROM produtos', (error, results) => { // linha de comando em SQL banco de dados
         if (error) {
             return res.status(500).json({ error: 'Erro ao buscar produtos' });
         }
-        res.json(results);
+        res.json(results); // resposta
     });
 });
 
